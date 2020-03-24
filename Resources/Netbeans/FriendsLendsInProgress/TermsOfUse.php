ab6853a3cd71326bbaf88c43fafd839900b107ea
Sandy Forrester <sandyforresteruk@gmail.com>
2020-03-24 20:53:32 +0000

    no message

---
+++ Resources/Netbeans/FriendsLendsInProgress/TermsOfUse.php
@@ -0,0 +1,71 @@
+
+
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <body>
+       
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="welcome.php">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="SARAH'S CREATE ITEM PAGE">Create new item</a>
+                <a class="nav-item nav-link" href="CONTACT.PHP">Contact</a>
+                <a class="nav-item nav-link" href="TERMSOFUSE.PHP">Terms of use</a>
+                <a class="nav-item nav-link" href="MYDASHBOARD.PHP">My account dashboard</a>
+                <a class="nav-item nav-link" href="logout.php">Log out</a>
+            </div>
+        </nav>
+  </body>
+<style>
+div {
+  margin-bottom: 15px;
+  padding: 4px 12px;
+}
+.info {
+  background-color: #e7f3fe;
+  border-left: 6px solid #2196F3;
+}
+
+.success {
+  background-color: #ddffdd;
+  border-left: 6px solid #4CAF50;
+}
+.warning {
+  background-color: #ffffcc;
+  border-left: 6px solid #ffeb3b;
+}
+
+.danger {
+  background-color: #ffdddd;
+  border-left: 6px solid #f44336;
+}
+
+</style>
+</head>
+<body>
+
+<h2>Terms Of Use</h2>
+<div class="info">
+  <p><strong>Info!</strong> In using our website and our services, you agree to abide by our terms of use. 
+      Legal action will be taken if any user missuses the purpose of this service thank you.</p>
+</div>
+
+<div class="success">
+  <p><strong>Success!</strong> FriendsLends is a friendly user service aimed at group of friends.
+  Our success is making sure your needs are met by sharing and lending with friend. In our services we are ethically minded and we are doing our part to 
+  "SAVE THE PLANET".</p>
+</div>
+
+<div class="warning">
+  <p><strong>Warning!</strong> Rubber plants must be kept in a bright place and watered if needed.
+      Please do not feed the rabbits anything other than rabbit food.</p>
+</div>
+
+<div class="danger">
+  <p><strong>Danger!</strong> Please be careful when you carry heavyweights items.</p>
+</div>
+
+</body>
+</html>
+
+        
